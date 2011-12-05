3f6d96372aaf9d20475f5dc682a94f8ca269a9b6
Patrick Drechsler/Win7 <patrick@pdrechsler.de>
2011-12-05 18:46:33 +0100

    added rhinomocks added empty tests

---
+++ 4.Realize/Src/GradeGrinder/GradeGrinder.Gui.StatusBarModule.Test/TestStatusBarModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GradeGrinder.Gui.StatusBarModule.Test
+{
+    [TestFixture]
+    public class TestStatusBarModule
+    {
+    }
+}
